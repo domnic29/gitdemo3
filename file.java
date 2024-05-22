343f1f5c7c2dbd40bb038a291a8c273333715e3c
root <root@ip-172-31-8-193.ap-south-1.compute.internal>
2024-05-22 09:13:00 +0000

    this is my second commit

--- file.java
+++ file.java
@@ -29,9 +29,10 @@ public static void main(String args[])
 {  
 Scanner sc = new Scanner(System.in);  
 System.out.print("Enter the value of n: ");  
-int n = sc.nextInt();  
 System.out.println();  
 //function calling  
 printPattern(n);  
 }  
-}  
+} 
+
+this is my second commit
